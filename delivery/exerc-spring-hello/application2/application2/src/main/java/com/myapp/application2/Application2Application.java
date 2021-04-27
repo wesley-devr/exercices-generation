fa6273cffe8afa07767f95dbcdc54e18438ce1ee
Wesley-devr <wesleyalexenv@gmail.com>
2021-04-27 18:27:59 -0300

    add folder app

---
+++ delivery/exerc-spring-hello/application2/application2/src/main/java/com/myapp/application2/Application2Application.java
@@ -0,0 +1,21 @@
+package com.myapp.application2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application2Application.class, args);
+	}
+	
+	@GetMapping("/app2")
+	public String printObjectiveLearning() {
+		return "Learn Spring Boot, Api Rest, Protocol Http, Status Code, Model Mvc.";
+	}
+
+}
