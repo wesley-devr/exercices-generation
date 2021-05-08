b70e58101dbdb03025558f801c6359ec69abc144
Wesley-devr <wesleyalexenv@gmail.com>
2021-05-08 04:19:06 -0300

    Atualizando o projeto

---
+++ deliver/blogPessoal/blogPessoal/src/main/java/com/blogpessoal/blogPessoal/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.blogpessoal.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blogpessoal.blogPessoal.model.UserLogin;
+import com.blogpessoal.blogPessoal.model.Usuario;
+import com.blogpessoal.blogPessoal.services.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UserController {
+
+	@Autowired
+	private UsuarioService usuarioServices;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioServices.logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioServices.cadastrarUsuario(usuario));
+	} 
+}
---
+++ deliver/blogPessoal/blogPessoal/src/main/java/com/blogpessoal/blogPessoal/security/UserDetailsImpl.java
@@ -0,0 +1,64 @@
+package com.blogpessoal.blogPessoal.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.blogpessoal.blogPessoal.model.Usuario;
+
+public class UserDetailsImpl implements UserDetails {
+	
+	private static final long serialVersionUID = 1L; 
+	
+	private String userName;
+	private String password;
+	
+	public UserDetailsImpl (Usuario userName, Usuario password) {
+		this.userName = userName.getNome();
+		this.password = password.getSenha();
+	}
+	
+	public UserDetailsImpl () {
+		
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true; // como uma boa prática definir como true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+	
+		return true;
+	}
+
+	
+}
